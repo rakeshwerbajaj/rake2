22ffe396f5ff4cf0843e55d0e4b265f1b11edb49
rakeshwerbajaj <rakeshwerbajaj@gmail.com>
2023-04-10 15:58:38 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# rake2
+# Dash Default Template: MyDashApp
+
+Created on 2023-04-07 22:35:11.249715
+
+Welcome to your [Plotly Dash](https://plotly.com/dash/) App! This is a template for your MyDashApp app.
+
+See the [Dash Documentation](https://dash.plotly.com/introduction) for more information on how to get your app up and running.
+
+## Running the App
+
+Run `src/app.py` and navigate to http://127.0.0.1:8050/ in your browser.
---
+++ src/app.py
@@ -0,0 +1,37 @@
+'''
+ # @ Create Time: 2023-04-07 22:35:11.251056
+'''
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, title="MyDashApp")
+
+# Declare server for Heroku deployment. Needed for Procfile.
+server = app.server
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
